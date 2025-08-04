d04b16ca2e74b7257e1fd3b88e15ace6c0b4f40f
MidhileshDevops <MidhileshPardhuDevops@gmail.com>
2025-08-04 22:05:29 +0530

    Update add.py

--- add.py
+++ add.py
@@ -1,14 +1,14 @@
 # To take input from the user
-#n = int(input("Enter a number: "))
+#num = int(input("Enter a number: "))
 
 factorial = 1
 
 # check if the number is negative, positive or zero
-if n < 0:
+if num < 0:
    print("Sorry, factorial does not exist for negative numbers")
-elif n == 0:
+elif num == 0:
    print("The factorial of 0 is 1")
 else:
-   for i in range(1,n + 1):
+   for i in range(1,num + 1):
        factorial = factorial*i
-   print("The factorial of",n,"is",factorial)
+   print("The factorial of",num,"is",factorial)
